6b71ac1e10394f648a663228fc07d1fd6e9f23e3
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-27 17:13:52 +0300

    Feture: add routes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,17 @@
 import { Application } from './components/application';
+import { NativeRouter, Route, Routes } from 'react-router-native';
+import React from 'react';
+import { NotFound } from './components/not_found';
+import { OrganizationDetails } from './components/organization';
 
 export default function App() {
-  return <Application />;
+  return (
+    <NativeRouter>
+      <Routes>
+        <Route path="/" element={<Application />} /> 
+        <Route path="/org/:name/:id" element={<OrganizationDetails />} /> 
+        <Route path="*" element={<NotFound />} /> 
+      </Routes>              
+    </NativeRouter>
+  );
 }
