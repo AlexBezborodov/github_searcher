a5285cc9648a011573e0a303baf1e870dc95b39e
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-27 17:10:53 +0300

    Feature: add about company component

---
+++ components/organization/about/about_company.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { View } from 'react-native';
+import { Card, Subheading, Title } from 'react-native-paper';
+
+export const AboutCompany: FC<any> = (data) => { 
+    return (
+    <View style={{ padding: 1, marginVertical: 16}}>
+      <Card style={{marginVertical: 4}} >
+        <Card.Content>
+          <Title>
+              Name: {data.name || "no info"}
+          </Title>
+          <Subheading>Location:{ data.location || "not filled"}</Subheading>
+          <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
+            <Subheading>Followers: {data?.followers || 0}</Subheading>
+            <Subheading>Repos: {data?.public_repos || 0}</Subheading>
+          </View>
+        </Card.Content>  
+      </Card>  
+    </View>
+          
+  )
+}
